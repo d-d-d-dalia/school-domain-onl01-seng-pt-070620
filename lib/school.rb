47b50a882f802118b2bef307f6bd65f78b909358
Dalia Sawaya <dalia.sawaya@flatironschool.com>
2020-08-03 22:20:16 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+require 'pry'
+class School
+  
+  attr_accessor :roster
+  
+  def initialize(school)
+    @roster = {}
+    
+  end
+  
+  def add_student(name, grade)
+    if @roster[grade] == nil
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
+  
+  def grade(num)
+    @roster[num]
+  end
+  
+  def sort
+    new_roster = {}
+    @roster.collect do |key, value|
+      new_roster[key] = value.sort
+    end
+    new_roster
+  end
+  
+  
+end
